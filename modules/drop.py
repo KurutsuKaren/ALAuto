d2436750fa5d3ef3b01d26d07d40de20b83995a7
kurutsukaren <kurutsukarenal@gmail.com>
2019-10-19 20:00:09 +0200

    Drops list added

--- modules/drop.py
+++ modules/drop.py
@@ -13,11 +13,14 @@ from util.utils import Utils
 class DropManager(object):
 
     pool = [5, 6, 11, 14, 15, 16, 17, 19, 43, 44, 52, 53, 72, 75, 76, 86, 87, 91, 101, 102, 167, 224, 225, 236, 238, 239, 240]
+    drops = []
 
     @staticmethod
     def find_droped():
         for p in DropManager.pool:
             if Utils.find("ships/"+str(p)):
+                DropManager.drops.append(p)
+                print(DropManager.drops)
                 return p
 
         return -1
